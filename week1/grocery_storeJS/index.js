7eb0ed68d42120d831573aa625c0bd33158adaa2
chadpiper <chad.piper@bryanuniversity.edu>
2020-07-31 15:17:10 -0700

    Add files via upload

---
+++ week1/grocery_storeJS/index.js
@@ -0,0 +1,15 @@
+
+//object
+var shopper = {
+    firstName: "Chad",
+    lastName: "Piper",
+    age: 36,
+    isShopping: true,
+    // Array
+    groceryCart:["bread", "hotdog", "mustard", "ketchup"],
+    // function
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+
+};
